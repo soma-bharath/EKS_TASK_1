fbbafc7dd5215077f7ec38f7135af6c99790f866
Soma Bharath Kumar <somabharath777@gmail.com>
2024-04-02 15:16:15 +0530

    added elb code

---
+++ alb.tf
@@ -0,0 +1,33 @@
+resource "aws_lb" "my_load_balancer" {
+  name               = "my-elb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [for j in data.aws_subnet.public_subnets : j.id]
+}
+
+
+# Configure target group
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.main_vpc.id
+}
+
+# Attach target group to ELB listener
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
+}
+
+
+resource "aws_autoscaling_attachment" "eks_node_group_attachment" {
+  autoscaling_group_name = aws_eks_node_group.testeksclusternode.name
+  alb_target_group_arn   = aws_alb_target_group.my_target_group.arn
+}
