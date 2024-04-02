fbe9f1e56dfbb96d8910fc64eb9277117773bebf
Soma Bharath Kumar <somabharath777@gmail.com>
2024-04-02 15:31:57 +0530

    added data.tf

--- data.tf
+++ data.tf
@@ -25,3 +25,19 @@ data "aws_subnet" "private_subnets"{
 data "aws_security_group" "EKS-Security-Group"{
   name = "eks-cluster-sg"
 }
+
+data "aws_subnets" "public"{
+  filter {
+    name="vpc-id"
+    values=[data.aws_vpc.main_vpc.id]
+  }
+  filter {
+    name="tag:Subnet-Type"
+    values=["public"]
+  }
+}
+
+data "aws_subnet" "public_subnets"{
+  for_each=toset(data.aws_subnets.public.ids)
+  id = each.value
+}
