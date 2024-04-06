679a1da0103a9ba7f253c3807a1760a45dbffe3f
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-06 19:28:28 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -25,9 +25,10 @@ resource "aws_lb_listener" "my_listener" {
     target_group_arn = aws_lb_target_group.my_target_group.arn
   }
 }
-
+/*
 resource "aws_autoscaling_attachment" "eks_node_group_attachment" {
   autoscaling_group_name = data.aws_autoscaling_groups.eks_asg.name
   lb_target_group_arn   = aws_alb_target_group.my_target_group.arn
 
 }
+*/
