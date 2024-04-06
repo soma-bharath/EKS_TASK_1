566051a7b68b6c6061421349cb9e50fb08cf5c95
Soma Bharath Kumar <somabharath777@gmail.com>
2024-04-06 17:23:52 +0530

    updated code

--- EKS-Node.tf
+++ EKS-Node.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_node_group" "testeksclusternode" {
   cluster_name    = aws_eks_cluster.testekscluster.name
   node_group_name = "test-eks-nodegroup-1"
-  release_version        = "1.28.1-20231002"
+  #release_version        = "1.28.1-20231002"
   node_role_arn          = aws_iam_role.Amazon_EKS_NodeRole.arn
   subnet_ids             = [for s in data.aws_subnet.private_subnets : s.id]
   tags = {
