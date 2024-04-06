38506a1bbfb9d6826864281537b1b7044a26e394
Soma Bharath Kumar <somabharath777@gmail.com>
2024-04-06 16:06:25 +0530

    updated code

---
+++ EKS-Node.tf
@@ -0,0 +1,27 @@
+resource "aws_eks_node_group" "testeksclusternode" {
+  cluster_name    = aws_eks_cluster.testekscluster.name
+  node_group_name = "test-eks-nodegroup-1"
+  release_version        = "1.28.1-20231002"
+  subnet_ids             = [for s in data.aws_subnet.private_subnets : s.id]
+  tags = {
+    Name = "test-eks-nodegroup-1"
+  }
+  tags_all = {
+    Name = "test-eks-nodegroup-1"
+  }
+
+  version = "1.28"
+  scaling_config {
+    desired_size = 3
+    max_size     = 6
+    min_size     = 2
+  }
+  update_config {
+    max_unavailable = 1
+  }
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+   depends_on = [aws_eks_cluster.testekscluster]
+}
---
+++ launch_template.tf
@@ -0,0 +1,38 @@
+resource "aws_launch_template" "example" {
+  name_prefix   = "example-lt"
+  image_id      = "ami-0395649fbe870727e"
+  instance_type = "t2.micro"
+
+  iam_instance_profile {
+    arn = aws_iam_role.Amazon_EKS_NodeRole.arn
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              mkdir /home/ec2-user/localpath
+              sudo yum update -y
+              sudo yum install -y docker
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              cat <<EOFR | sudo tee /etc/yum.repos.d/kubernetes.repo
+              [kubernetes]
+              name=Kubernetes
+              baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+              enabled=1
+              gpgcheck=1
+              gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+              EOFR
+              sudo yum install -y kubectl
+              sudo aws eks --region us-west-2 update-kubeconfig --name test-eks-cluster
+              kubectl create namespace kumar
+              EOF
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 30
+      volume_type = "gp2"
+    }
+  }
+}
