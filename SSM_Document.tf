21ef84bde386cc4a3ee4bd66abd3b27aa417cff5
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-08 15:18:48 +0530

    Update SSM_Document.tf

--- SSM_Document.tf
+++ SSM_Document.tf
@@ -3,9 +3,17 @@
 variable "shell_script_content" {
   default = <<-EOT
 #!/bin/bash
-
-echo "Hello from Terraform SSM!"
-echo "This is a shell script running on the instance."
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+EOF
+sudo yum install -y kubectl
+sudo aws eks --region us-west-2 update-kubeconfig --name test-eks-cluster
+sudo kubectl create namespace kumar
 EOT
 }
 
