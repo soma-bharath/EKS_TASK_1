d47bb8b9e6020b99556d62df3de75929f9298ee1
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-08 17:51:53 +0530

    Update SSM_Document.tf

--- SSM_Document.tf
+++ SSM_Document.tf
@@ -5,7 +5,7 @@ resource "aws_ssm_document" "document" {
   content = <<DOC
   {
     "schemaVersion": "1.2",
-    "description": "Check ip configuration of a Linux instance.",
+    "description": "Check ip configuration of a Node instance.",
     "parameters": {
 
     },
@@ -14,7 +14,7 @@ resource "aws_ssm_document" "document" {
         "properties": [
           {
             "id": "0.aws:runShellScript",
-            "runCommand": ["#!/bin/bash","cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo","[kubernetes]","name=Kubernetes","baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/","enabled=1","gpgcheck=1","gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key","EOF","sudo yum install -y kubectl","sudo yum install -y docker","sudo systemctl start docker","sudo systemctl enable docker","sudo docker pull python"]
+            "runCommand": ["#!/bin/bash","cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo","[kubernetes]","name=Kubernetes","baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/","enabled=1","gpgcheck=1","gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key","EOF","sudo yum install -y kubectl","sudo aws eks --region us-west-2 update-kubeconfig --name test-eks-cluster"]
           }
         ]
       }
@@ -26,6 +26,7 @@ DOC
 resource "aws_ssm_association" "associate"{
 name = aws_ssm_document.document.name
 targets {
+
     key    = "tag: Name"
     values = ["test-eks-nodegroup-1"]
 }
