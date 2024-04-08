2caafc008aa4ec39adeb30c5fc7e36ce259d589a
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-08 15:33:39 +0530

    Update SSM_Document.tf

--- SSM_Document.tf
+++ SSM_Document.tf
@@ -6,24 +6,21 @@ data "template_file" "shell_script_content" {
 resource "aws_ssm_document" "shell_script" {
   name          = "RunShellScript"
   document_type = "Command"
-  content = <<-EOF
-{
-  "schemaVersion": "1.0",
-  "description": "Runs a shell script",
-  "runtimeConfig": {
-    "aws:runShellScript": {
-      "properties": [
-        {
-          "id": "0.aws:runShellScript",
-          "shellScript": "${data.template_file.shell_script_content.rendered}"
-        }
-      ]
+  content = jsonencode({
+    schemaVersion = "1.0"
+    description   = "Runs a shell script"
+    runtimeConfig = {
+      "aws:runShellScript" = {
+        properties = [
+          {
+            id          = "0.aws:runShellScript"
+            shellScript = data.template_file.shell_script_content.rendered
+          }
+        ]
+      }
     }
-  }
+  })
 }
-EOF
-}
-
 # Create an association to execute the shell script
 resource "aws_ssm_association" "shell_script_association" {
   name          = "run-shell-script"
