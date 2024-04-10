3b8321221e76a56c487ae109211a4cd3ef541a31
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-10 18:57:54 +0530

    Update EKS-Cluster-node.tf

--- EKS-Cluster-node.tf
+++ EKS-Cluster-node.tf
@@ -14,7 +14,7 @@ resource "aws_eks_node_group" "testeksclusternode" {
   subnet_ids             = [for s in data.aws_subnet.private_subnets : s.id]
 
   remote_access {
-    ec2_ssh_key               = var.key_name
+    ec2_ssh_key               = aws_key_pair.Node_key_pair.name
     source_security_group_ids = [data.aws_security_group.EKS-Security-Group.id]
   }
   tags = {
