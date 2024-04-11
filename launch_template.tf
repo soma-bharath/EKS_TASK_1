c6d38f8441318df8aa9562ed355c289f76f115f2
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-11 19:46:01 +0530

    Update launch_template.tf

--- launch_template.tf
+++ launch_template.tf
@@ -3,15 +3,29 @@ resource "aws_launch_template" "example" {
 
   key_name      = aws_key_pair.Node_key_pair.key_name
 
-  user_data = filebase64("${path.module}/kubesetup.sh")
-  
+  #user_data = filebase64("${path.module}/kubesetup.sh")
+
+security_group_names = [aws_security_group.example_sg.name]
+
   block_device_mappings {
     device_name = "/dev/xvda"
-
     ebs {
-      volume_size = 30
+      volume_size           = 30
       volume_type = "gp2"
+      delete_on_termination = true
+      encrypted             = true
     }
   }
+
+  block_device_mappings {
+    device_name = "/dev/sdh"
+    ebs {
+      volume_size           = 50
+      volume_type = "gp2"
+      delete_on_termination = true
+      encrypted             = true
+    }
+  }
+
    depends_on = [aws_eks_cluster.testekscluster]
 }
