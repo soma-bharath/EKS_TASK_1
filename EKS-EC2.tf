7a82e4f18a36cc9c20ea03c1d33b990a8ac2fc5a
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-12 11:20:56 +0530

    Update EKS-EC2.tf

--- EKS-EC2.tf
+++ EKS-EC2.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "my_ec2" {
   instance_type   = "t2.micro"
   ami             = "ami-0395649fbe870727e"
-  #subnet_id       = aws_subnet.public_subnet_1.id
-  subnet_ids             = [for s in data.aws_subnet.private_subnets : s.id]
+  subnet_id       = data.aws_subnet.public_subnet_1.id
   vpc_security_group_ids = [data.aws_security_group.EKS-Security-Group.id]
   key_name        = aws_key_pair.Node_key_pair.key_name
  iam_instance_profile = aws_iam_instance_profile.EKS-EC2.name
