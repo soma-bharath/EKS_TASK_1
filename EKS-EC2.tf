501b7143664025cf347451dc378f5c5df270c74e
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-12 10:52:57 +0530

    Update EKS-EC2.tf

--- EKS-EC2.tf
+++ EKS-EC2.tf
@@ -1,19 +1,20 @@
 resource "aws_instance" "my_ec2" {
   instance_type   = "t2.micro"
-  ami             = "ami-0b0dcb5067f052a63"
-  subnet_id       = aws_subnet.public_subnet_1.id
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
-  key_name        = "TF"
+  ami             = "ami-0395649fbe870727e"
+  #subnet_id       = aws_subnet.public_subnet_1.id
+  subnet_ids             = [for s in data.aws_subnet.private_subnets : s.id]
+  vpc_security_group_ids = [data.aws_security_group.EKS-Security-Group.id]
+  key_name        = aws_key_pair.Node_key_pair.key_name
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = tls_private_key.rsa.private_key_pem
+    private_key = tls_private_key.keypair.private_key_pem
     host        = aws_instance.my_ec2.public_ip
   }
 
     provisioner "file" {
-    source      = "${path.module}/TF.pem"
-    destination = "/home/ec2-user/.ssh/TF.pem"
+    source      = "${path.module}/Node-key.pem"
+    destination = "/home/ec2-user/.ssh/Node-key.pem"
   }
 
     provisioner "file" {
@@ -33,10 +34,6 @@ gpgcheck=1
 gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
 EOFR
 sudo yum install -y kubectl
-sudo yum install -y docker
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo docker pull python
 sudo chmod 700 /home/ec2-user/.ssh/TF.pem
 sudo cp ${path.module}/configHost ~/.ssh/
 EOF
