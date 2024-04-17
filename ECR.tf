9bfe9065ccc8f0d68ea2e37fcbb98c02117b639b
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-17 10:16:39 +0530

    Update ECR.tf

--- ECR.tf
+++ ECR.tf
@@ -1,9 +1,4 @@
 
 resource "aws_ecr_repository" "my_repository" {
   name = "my-ecr-repository"
-  tags = {
-    Name = "my-ecr-repository"
-    Date = local.current_date
-    Env  = var.env
-  }
 }
