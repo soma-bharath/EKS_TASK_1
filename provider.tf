3fcefce68600f3504e8709b6475e7122a26780a1
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-20 20:17:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -23,6 +23,7 @@ resource "null_resource" "kubectl_setup" {
       sudo bash ${path.module}/kubesetup.sh
       sudo kubectl apply -f ${path.module}/cluster-autoscaler-autodiscover.yaml
       sudo kubectl -n kube-system annotate deployment.apps/cluster-autoscaler cluster-autoscaler.kubernetes.io/safe-to-evict="false"
+      sudo bash ${path.module}/argocd.sh
     EOT
   }
   depends_on = [aws_eks_cluster.testekscluster, aws_eks_node_group.testeksclusternode]
